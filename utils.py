2389dbda21e9e3358e55e4e1d331e3b87fe11df2
DanielPerezF <daniel.perezf24@gmail.com>
2023-05-13 21:58:20 +0200

    Added gini and adjusted importing function

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from gams.transfer import Container
 import pandas as pd
+import numpy as np
 
 def read_gdx(path_gdx: str,vars,initial_year=2015,tstep=5):
     """Read the gdx
@@ -18,11 +19,10 @@ def read_gdx(path_gdx: str,vars,initial_year=2015,tstep=5):
         if i != 0:
             df[var] = gdx[var].records['level']
 
-    df['t_0'] = df['t_0'].astype('int')
-    df['year'] = initial_year + tstep*(df['t_0']-1)
-    if 'n_1' in df.columns:
-        df['n_1'] = df['n_1'].astype(str)
-        df.rename(columns={'n_1':'region'},inplace=True)
+    df = df.rename(columns={'t_0':'t','n_1':'region'})
+    df['t'] = df['t'].astype('int')
+    df['year'] = initial_year + tstep*(df['t']-1)
+    df['region'] = df['region'].astype(str)
     return df
 
 def read_scenarios(paths,vars,initial_year=2015,tstep=5):
@@ -30,7 +30,7 @@ def read_scenarios(paths,vars,initial_year=2015,tstep=5):
 
     for path in paths:
         df_new = read_gdx(path,vars,initial_year,tstep)
-        df_new['scen'] = path.replace("results/results_","").replace(".gdx","")
+        df_new['scen'] = path.split('/')[-1].replace("results_",'').replace('.gdx','').replace('approach_','')
         df_old = pd.concat([df_old,df_new])
         
     return df_old
@@ -45,3 +45,25 @@ def calc_cum_emis(data,final_y=2100):
         cum_emis_scen['scen'] = scen
         cum_emis = pd.concat([cum_emis,cum_emis_scen])
     return cum_emis
+
+def gini(array):
+    """Calculate the Gini coefficient of a numpy array."""
+    # based on bottom eq:
+    # http://www.statsdirect.com/help/generatedimages/equations/equation154.svg
+    # from:
+    # http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+    # All values are treated equally, arrays must be 1d:
+    # array = array.flatten()
+    if np.amin(array) < 0:
+        # Values cannot be negative:
+        array -= np.amin(array)
+    # Values cannot be 0:
+    array = array + 0.0000001
+    # Values must be sorted:
+    array = np.sort(array)
+    # Index per array element:
+    index = np.arange(1,array.shape[0]+1)
+    # Number of array elements:
+    n = array.shape[0]
+    # Gini coefficient:
+    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array)))
